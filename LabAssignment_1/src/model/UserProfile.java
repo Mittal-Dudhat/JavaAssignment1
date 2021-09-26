57802012441d55930078b7c7790f5fef974478ba
Mital-Dudhat <dudhat.m@northeastern.edu>
2021-09-26 13:18:13 -0400

    java Assignment

---
+++ LabAssignment_1/src/model/UserProfile.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Mital
+ */
+public class UserProfile {
+    private String name;
+    private String geodata;
+    private String dob;
+    private Long teleNumber;
+    private Long faxNo;
+    private String emailId;
+    private String ssnNo;
+    private Long medRecNo;
+    private Long healthPlanNo;
+    private Long bankAccNo;
+    private String licenseNo;
+    private String vehId;
+    private String deviceId;
+    private String linkedIn;
+    private String ipAdd;
+    private String uniqueNo;
+    private String photoFileName;
+    private String biometricFileName;
+
+    public String getPhotoFileName() {
+        return photoFileName;
+    }
+
+    public void setPhotoFileName(String photoFileName) {
+        this.photoFileName = photoFileName;
+    }
+
+    public String getBiometricFileName() {
+        return biometricFileName;
+    }
+
+    public void setBiometricFileName(String biometricFileName) {
+        this.biometricFileName = biometricFileName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeodata() {
+        return geodata;
+    }
+
+    public void setGeodata(String geodata) {
+        this.geodata = geodata;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public Long getTeleNumber() {
+        return teleNumber;
+    }
+
+    public void setTeleNumber(Long teleNumber) {
+        this.teleNumber = teleNumber;
+    }
+
+    public Long getFaxNo() {
+        return faxNo;
+    }
+
+    public void setFaxNo(Long faxNo) {
+        this.faxNo = faxNo;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getSsnNo() {
+        return ssnNo;
+    }
+
+    public void setSsnNo(String ssnNo) {
+        this.ssnNo = ssnNo;
+    }
+
+    public Long getMedRecNo() {
+        return medRecNo;
+    }
+
+    public void setMedRecNo(Long medRecNo) {
+        this.medRecNo = medRecNo;
+    }
+
+    public Long getHealthPlanNo() {
+        return healthPlanNo;
+    }
+
+    public void setHealthPlanNo(Long healthPlanNo) {
+        this.healthPlanNo = healthPlanNo;
+    }
+
+    public Long getBankAccNo() {
+        return bankAccNo;
+    }
+
+    public void setBankAccNo(Long bankAccNo) {
+        this.bankAccNo = bankAccNo;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public String getVehId() {
+        return vehId;
+    }
+
+    public void setVehId(String vehId) {
+        this.vehId = vehId;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIpAdd() {
+        return ipAdd;
+    }
+
+    public void setIpAdd(String ipAdd) {
+        this.ipAdd = ipAdd;
+    }
+
+    public String getUniqueNo() {
+        return uniqueNo;
+    }
+
+    public void setUniqueNo(String uniqueNo) {
+        this.uniqueNo = uniqueNo;
+    }
+    
+    
+}
